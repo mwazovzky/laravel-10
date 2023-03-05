7701026613ff692551518b77bdc75f9d002e4909
Mike Wazovzky <mike.wazovzky@gmail.com>
2023-03-05 17:56:53 +0400

    Create Currency model

---
+++ database/seeders/CurrencySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Currency;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CurrencySeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Currency::factory()->count(10)->create();
+    }
+}
